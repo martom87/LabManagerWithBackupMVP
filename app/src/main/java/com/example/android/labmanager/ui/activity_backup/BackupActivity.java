aa9bd198afba5e2f6c6a7352d11588d63a703357
martom87 <marcintomala87@gmail.com>
2018-02-25 17:34:58 +0100

    added BackupMenu

--- app/src/main/java/com/example/android/labmanager/ui/activity_backup/BackupActivity.java
+++ app/src/main/java/com/example/android/labmanager/ui/activity_backup/BackupActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.labmanager.ui.activity_backup;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -14,6 +17,8 @@ import com.example.android.labmanager.App;
 import com.example.android.labmanager.R;
 import com.example.android.labmanager.adapters.BackupAdapter;
 import com.example.android.labmanager.db.LabManagerBackup;
+import com.example.android.labmanager.ui.activity_menu.MenuActivity;
+import com.example.android.labmanager.ui.activity_menu.MenuDrawer;
 import com.github.paolorotolo.expandableheightlistview.ExpandableHeightListView;
 import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveId;
@@ -26,29 +31,37 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Optional;
 
 
-public class BackupActivity extends AppCompatActivity implements BackupView {
+public class BackupActivity extends MenuActivity implements BackupView, MenuDrawer {
 
-
+    @Optional
     @OnClick(R.id.activity_backup_drive_button_backup)
     public void onClick() {
 
         backupPresenter.openFolderPicker(true, this);
     }
 
+    @Optional
     @OnClick(R.id.activity_backup_drive_button_manage_drive)
     public void onClick2() {
 
         backupPresenter.openOnDrive(DriveId.decodeFromString(backupPresenter.getBackupFolder()), this);
     }
 
+    @BindView(R.id.textViewToolbar)
+    TextView textViewToolbar;
+
+    @Nullable
     @BindView(R.id.activity_backup_drive_button_manage_drive)
     TextView manageButton;
 
+    @Nullable
     @BindView(R.id.activity_backup_drive_textview_folder)
     TextView folderTextView;
 
+    @Optional
     @OnClick(R.id.activity_backup_drive_button_folder)
 
     public void onClick3() {
@@ -59,6 +72,7 @@ public class BackupActivity extends AppCompatActivity implements BackupView {
         }
     }
 
+    @Nullable
     @BindView(R.id.activity_backup_drive_listview_restore)
     ExpandableHeightListView backupListView;
 
@@ -69,14 +83,13 @@ public class BackupActivity extends AppCompatActivity implements BackupView {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.backup_drive_activity);
+        invokeMenuDrawer();
         ButterKnife.bind(this);
         ((App) getApplication()).getAppComponent().inject(this);
-
+        setTitle();
         backupPresenter.attachBackupView(this);
         backupPresenter.initialize(this);
-        //   backupPresenter.showBackupFolder();
-        //   backupPresenter.populateBackupList();
+
 
         makeBackUpListViewExpandalbe();
 
@@ -89,6 +102,19 @@ public class BackupActivity extends AppCompatActivity implements BackupView {
         backupPresenter.detachBackupView();
         backupPresenter.disconnectClient();
         super.onDestroy();
+    }
+
+    @Override
+    public void invokeMenuDrawer() {
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.backup_drive_activity, null, false);
+        drawer.addView(contentView, 0);
+    }
+
+    @Override
+    public void setTitle() {
+        textViewToolbar.setText("Backup");
     }
 
 
@@ -156,9 +182,9 @@ public class BackupActivity extends AppCompatActivity implements BackupView {
                     DriveId mFolderDriveId = data.getParcelableExtra(
                             OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
 
-                  backupPresenter.saveBackupFolder(mFolderDriveId.encodeToString());
+                    backupPresenter.saveBackupFolder(mFolderDriveId.encodeToString());
 
-                   backupPresenter.uploadToDrive(mFolderDriveId, this);
+                    backupPresenter.uploadToDrive(mFolderDriveId, this);
                 }
                 break;
 
@@ -170,7 +196,7 @@ public class BackupActivity extends AppCompatActivity implements BackupView {
                             OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
 
                     DriveFile file = driveId.asDriveFile();
-                   backupPresenter.downloadFromDrive(file);
+                    backupPresenter.downloadFromDrive(file);
 
                 } else {
                     showErrorDialog();

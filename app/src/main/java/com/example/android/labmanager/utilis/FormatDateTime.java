8aebcc9ef5182c398de36fcfc3b1f1d581e8a153
martom87 <marcintomala87@gmail.com>
2018-03-22 22:00:48 +0100

    tiding up 2

--- app/src/main/java/com/example/android/labmanager/utilis/FormatDateTime.java
+++ app/src/main/java/com/example/android/labmanager/utilis/FormatDateTime.java
@@ -5,11 +5,8 @@ package com.example.android.labmanager.utilis;
 import android.content.Context;
 
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-
 
 
 public class FormatDateTime {
@@ -20,29 +17,6 @@ public class FormatDateTime {
         this.context = mContext;
     }
 
-    public String convertDateTime(String date) {
-
-        DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        DateFormat finalDataFormat = DateFormat.getDateInstance(DateFormat.SHORT);
-        DateFormat finalTimeFormat;
-
-        if (android.text.format.DateFormat.is24HourFormat(context)) {
-            finalTimeFormat = new SimpleDateFormat("HH:mm");
-        } else {
-            finalTimeFormat = new SimpleDateFormat("hh:mm a");
-        }
-
-        Date parsed = null;
-        try {
-            parsed = inputFormat.parse(date);
-        } catch (ParseException e) {
-
-            e.printStackTrace();
-        }
-        String finalData = finalDataFormat.format(parsed);
-        String finalTime = finalTimeFormat.format(parsed);
-        return finalData + " " + finalTime;
-    }
 
     public String formatDate(Date date) {
         DateFormat finalDataFormat = DateFormat.getDateInstance(DateFormat.SHORT);
@@ -57,97 +31,6 @@ public class FormatDateTime {
         String finalData = finalDataFormat.format(date);
         String finalTime = finalTimeFormat.format(date);
         return finalData + " " + finalTime;
-    }
-
-    public String convertDateToMonthOverview(String date) {
-        DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        DateFormat finalDataFormat = new SimpleDateFormat("MMMM");
-
-        Date parsed = null;
-        try {
-            parsed = inputFormat.parse(date);
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(parsed);
-            cal.add(Calendar.MONTH, -1);
-            parsed = cal.getTime();
-        } catch (ParseException e) {
-
-            e.printStackTrace();
-        }
-        String finalData = finalDataFormat.format(parsed);
-        return finalData + " ";
-    }
-
-
-    public String convertDate(String datetime) {
-        DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd");
-        DateFormat finalDataFormat = DateFormat.getDateInstance(DateFormat.SHORT);
-
-        Date parsed = null;
-        try {
-            parsed = inputFormat.parse(datetime);
-        } catch (ParseException e) {
-
-            e.printStackTrace();
-        }
-        String finalData = finalDataFormat.format(parsed);
-        return finalData + "";
-    }
-
-    public String convertRawDate(Date datetime) {
-        DateFormat finalDataFormat = DateFormat.getDateInstance(DateFormat.SHORT);
-
-        return finalDataFormat.format(datetime);
-    }
-
-    public String convertRawTime(Date datetime) {
-        DateFormat finalTimeFormat;
-
-        if (android.text.format.DateFormat.is24HourFormat(context)) {
-            finalTimeFormat = new SimpleDateFormat("HH:mm");
-        } else {
-            finalTimeFormat = new SimpleDateFormat("hh:mm a");
-        }
-
-        return finalTimeFormat.format(datetime);
-    }
-
-    public String getCurrentTime() {
-        Calendar cal = Calendar.getInstance();
-
-        DateFormat finalTimeFormat;
-
-        finalTimeFormat = DateFormat.getTimeInstance(DateFormat.SHORT);
-
-
-        String finalTime = finalTimeFormat.format(cal.getTime());
-        return finalTime + "";
-    }
-
-    public String getCurrentDate() {
-        Calendar cal = Calendar.getInstance();
-
-        DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(context);
-
-        String finalTime = dateFormat.format(cal.getTime());
-        return finalTime + "";
-    }
-
-    public String getTime(Calendar cal) {
-        DateFormat finalTimeFormat;
-
-        finalTimeFormat = DateFormat.getTimeInstance(DateFormat.SHORT);
-
-
-        String finalTime = finalTimeFormat.format(cal.getTime());
-        return finalTime + "";
-    }
-
-    public String getDate(Calendar cal) {
-        DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(context);
-
-        String finalTime = dateFormat.format(cal.getTime());
-        return finalTime + "";
     }
 
 
